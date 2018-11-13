5346638280a27b2b19d9f3532171072e1353fd98
jason.wong <jason.wong@jasonwongs-MacBook-Pro.local>
2018-11-13 11:12:57 +0800

    2018-11-18a

--- wordpress/percolator_significant_terms_on_movies/script/02_set_fbeat_var.sh
+++
@@ -1,3 +0,0 @@
-export DATA_PATH=/Users/jason.wong/Development/elastic/blog/blog/wordpress/percolator_significant_terms_on_movies/data/movies.json
-export TMPL_PATH=/Users/jason.wong/Development/elastic/blog/blog/wordpress/percolator_significant_terms_on_movies/filebeat_config/movies.template.json
-
